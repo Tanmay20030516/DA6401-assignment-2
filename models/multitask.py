7f34c6931b5964af27f039f0d051712123593589
Tanmay <gawandeth@gmail.com>
2026-04-11 16:34:18 +0530

    feat: updated drive links for  one last try

--- models/multitask.py
+++ models/multitask.py
@@ -32,18 +32,18 @@ class MultiTaskPerceptionModel(nn.Module):
         super(MultiTaskPerceptionModel, self).__init__()
         import gdown
         import os
-        # classifier.pth = https://drive.google.com/file/d/1kWaCfUsk_o9Z0mCeDtKeOq79XdvDuw6r/view?usp=sharing
-        # localizer.pth = https://drive.google.com/file/d/1-7OpCdeKpC2XG4-B0vKVz2kuSghq-gu3/view?usp=sharing
-        # unet.pth = https://drive.google.com/file/d/1GuDwC3NaacmnjulLGgYnUQWCvhAwwFwC/view?usp=sharing
+        # classifier.pth = https://drive.google.com/file/d/15I3mpy9D4ukAtNX_BjRid3I28136ijiO/view?usp=sharing
+        # localizer.pth = https://drive.google.com/file/d/1wza6eOxyFmKpMYDxu4vcBsabntzR3Te_/view?usp=sharing
+        # unet.pth = https://drive.google.com/file/d/1peR8hoWoB28-Wl5MNVF0wcoGVXepBSGt/view?usp=sharing
         def _maybe_download(file_id: str, output: str):
             # avoid overwriting existing checkpoints if they are already present (e.g. from a previous run or manual download)
             if not os.path.exists(output):
                 gdown.download(id=file_id, output=output, quiet=False)
             else:
                 print(f"[checkpoint] using existing: {output}")
-        _maybe_download(file_id="1kWaCfUsk_o9Z0mCeDtKeOq79XdvDuw6r", output=classifier_path)
-        _maybe_download(file_id="1-7OpCdeKpC2XG4-B0vKVz2kuSghq-gu3", output=localizer_path)
-        _maybe_download(file_id="1GuDwC3NaacmnjulLGgYnUQWCvhAwwFwC", output=unet_path)
+        _maybe_download(file_id="15I3mpy9D4ukAtNX_BjRid3I28136ijiO", output=classifier_path)
+        _maybe_download(file_id="1wza6eOxyFmKpMYDxu4vcBsabntzR3Te_", output=localizer_path)
+        _maybe_download(file_id="1peR8hoWoB28-Wl5MNVF0wcoGVXepBSGt", output=unet_path)
         self.num_breeds = num_breeds
         self.seg_classes = seg_classes
         self.in_channels = in_channels
